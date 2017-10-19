0977225fd7e8e395c54034d15f3cdfa98adbb826
Austin Tinkel <austin.tinkel@gmail.com>
2017-10-19 09:41:30 -0400

    Created Mapper class and Keywords data structure. Started adding keywords to the Keywords class

---
+++ src/main/java/edu/bsu/cs222/Keywords.java
@@ -0,0 +1,40 @@
+package edu.bsu.cs222;
+
+public class Keywords {
+
+    public enum elfKeywords {
+        Elf, Nature, Fey, Feywild
+    }
+
+    public enum dwarfKeywords {
+
+    }
+
+    public enum gnomeKeywords {
+
+    }
+
+    public enum dragonbornKeywords {
+
+    }
+
+    public enum humanKeywords {
+
+    }
+
+    public enum halflingKeywords {
+
+    }
+
+    public enum halforcKeywords {
+
+    }
+
+    public enum tieflingKeywords {
+
+    }
+
+    public enum halfelfKeywords {
+
+    }
+}
---
+++ src/main/java/edu/bsu/cs222/Mapper.java
@@ -0,0 +1,6 @@
+package edu.bsu.cs222;
+
+public class Mapper {
+
+
+}
