651023b9eb17812eeadc460e07ff62babe1e9a95
Austin Tinkel <austin.tinkel@gmail.com>
2017-11-12 20:43:23 -0500

    Create tests for processResponseMap and start implementation

--- src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
+++ src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
@@ -1,6 +1,16 @@
 package edu.bsu.cs222.model;
 
+import java.util.HashMap;
+
 public class ResponseProcessor {
 
+    public KeywordDictionary processResponseMap(InputMap inputMap) {
+        HashMap<Integer, Response> map = inputMap.getInputMap();
 
+        for (int i = 0; i < 5; i++) {
+
+        }
+
+        return null;
+    }
 }
--- src/test/java/edu/bsu/cs222/TestResponseProcessor.java
+++ src/test/java/edu/bsu/cs222/TestResponseProcessor.java
@@ -1,11 +1,43 @@
 package edu.bsu.cs222;
 
+import edu.bsu.cs222.model.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestResponseProcessor {
 
     @Test
-    public void test() {
+    public void testProcessResponseMap_NotNull() {
+        ResponseProcessor responseProcessor = new ResponseProcessor();
 
+        InputMap inputMap = new InputMap();
+        inputMap.addInput(1, new Response("Sword"));
+        inputMap.addInput(2, new Response("Sword"));
+
+        KeywordDictionary responseKeywords = responseProcessor.processResponseMap(inputMap);
+        Assert.assertNotNull(responseKeywords);
+    }
+
+    @Test
+    public void testProcessResponseMap_Accuracy() {
+        ResponseProcessor responseProcessor = new ResponseProcessor();
+
+        InputMap inputMap = new InputMap();
+        inputMap.addInput(1, new Response("Fire Sword"));
+        inputMap.addInput(2, new Response("Fire Sword"));
+
+        KeywordDictionary expected = new KeywordDictionary();
+        KeywordList list1 = new KeywordList();
+        list1.add(new Keyword("Fire"));
+        list1.add(new Keyword("Sword"));
+        expected.add(list1);
+        KeywordList list2 = new KeywordList();
+        list2.add(new Keyword("Fire"));
+        list2.add(new Keyword("Sword"));
+        expected.add(list2);
+
+        KeywordDictionary actual = responseProcessor.processResponseMap(inputMap);
+
+        Assert.assertEquals(actual, expected);
     }
 }
