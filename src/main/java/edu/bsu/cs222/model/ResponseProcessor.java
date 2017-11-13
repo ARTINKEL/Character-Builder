9093a528282fd37db28d867b6da2d7c5f54de9f0
Austin Tinkel <austin.tinkel@gmail.com>
2017-11-12 20:52:30 -0500

    Split processResponseMap into two methods: One for race, one for class Create new tests for both

--- src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
+++ src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
@@ -1,16 +1,35 @@
 package edu.bsu.cs222.model;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import java.util.HashMap;
 
 public class ResponseProcessor {
 
-    public KeywordDictionary processResponseMap(InputMap inputMap) {
+    private KeywordParser keywordParser = new KeywordParser();
+
+    public KeywordDictionary processClassResponseMap(InputMap inputMap) throws UnirestException {
+
+        KeywordDictionary classResponses = new KeywordDictionary();
+
         HashMap<Integer, Response> map = inputMap.getInputMap();
 
-        for (int i = 0; i < 5; i++) {
-
+        for (int i = 0; i < 6; i++) {
+            KeywordList keywordList = keywordParser.extractKeywords(map.get(i).getResponse());
+            classResponses.add(keywordList);
         }
+        return classResponses;
+    }
 
-        return null;
+    public KeywordDictionary processRaceResponseMap(InputMap inputMap) throws UnirestException {
+        KeywordDictionary raceResponses = new KeywordDictionary();
+
+        HashMap<Integer, Response> map = inputMap.getInputMap();
+
+        for (int i = 6; i < 10; i++) {
+            KeywordList keywordList = keywordParser.extractKeywords(map.get(i).getResponse());
+            raceResponses.add(keywordList);
+        }
+        return raceResponses;
     }
 }
--- src/test/java/edu/bsu/cs222/TestResponseProcessor.java
+++ src/test/java/edu/bsu/cs222/TestResponseProcessor.java
@@ -1,5 +1,6 @@
 package edu.bsu.cs222;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import edu.bsu.cs222.model.*;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,19 +8,19 @@ import org.junit.Test;
 public class TestResponseProcessor {
 
     @Test
-    public void testProcessResponseMap_NotNull() {
+    public void testProcessClassResponseMap_NotNull() throws UnirestException {
         ResponseProcessor responseProcessor = new ResponseProcessor();
 
         InputMap inputMap = new InputMap();
         inputMap.addInput(1, new Response("Sword"));
         inputMap.addInput(2, new Response("Sword"));
 
-        KeywordDictionary responseKeywords = responseProcessor.processResponseMap(inputMap);
+        KeywordDictionary responseKeywords = responseProcessor.processClassResponseMap(inputMap);
         Assert.assertNotNull(responseKeywords);
     }
 
     @Test
-    public void testProcessResponseMap_Accuracy() {
+    public void testProcessClassResponseMap_Accuracy() throws UnirestException {
         ResponseProcessor responseProcessor = new ResponseProcessor();
 
         InputMap inputMap = new InputMap();
@@ -36,7 +37,42 @@ public class TestResponseProcessor {
         list2.add(new Keyword("Sword"));
         expected.add(list2);
 
-        KeywordDictionary actual = responseProcessor.processResponseMap(inputMap);
+        KeywordDictionary actual = responseProcessor.processClassResponseMap(inputMap);
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testProcessRaceResponseMap_NotNull() throws UnirestException {
+        ResponseProcessor responseProcessor = new ResponseProcessor();
+
+        InputMap inputMap = new InputMap();
+        inputMap.addInput(1, new Response("Sword"));
+        inputMap.addInput(2, new Response("Sword"));
+
+        KeywordDictionary responseKeywords = responseProcessor.processRaceResponseMap(inputMap);
+        Assert.assertNotNull(responseKeywords);
+    }
+
+    @Test
+    public void testProcessRaceResponseMap_Accuracy() throws UnirestException {
+        ResponseProcessor responseProcessor = new ResponseProcessor();
+
+        InputMap inputMap = new InputMap();
+        inputMap.addInput(1, new Response("Fire Sword"));
+        inputMap.addInput(2, new Response("Fire Sword"));
+
+        KeywordDictionary expected = new KeywordDictionary();
+        KeywordList list1 = new KeywordList();
+        list1.add(new Keyword("Fire"));
+        list1.add(new Keyword("Sword"));
+        expected.add(list1);
+        KeywordList list2 = new KeywordList();
+        list2.add(new Keyword("Fire"));
+        list2.add(new Keyword("Sword"));
+        expected.add(list2);
+
+        KeywordDictionary actual = responseProcessor.processRaceResponseMap(inputMap);
 
         Assert.assertEquals(actual, expected);
     }
