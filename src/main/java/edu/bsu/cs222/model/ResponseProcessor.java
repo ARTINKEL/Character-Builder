e88524104020913c92b267a1ad2cde3a9827586b
Austin Tinkel <austin.tinkel@gmail.com>
2017-11-30 09:34:16 -0500

    Fix "magic constant" problem

--- src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
+++ src/main/java/edu/bsu/cs222/model/ResponseProcessor.java
@@ -6,13 +6,17 @@ import java.util.HashMap;
 
 public class ResponseProcessor {
 
+    private final int classBeginningIndex = 1;
+    private final int classEndingIndex = 6;
+    private final int raceEndingIndex = 10;
+
     private KeywordExtractor keywordExtractor = new KeywordExtractor();
 
     public KeywordDictionary processClassResponseMap(HashMap<Integer, Response> inputMap) throws UnirestException {
 
         KeywordDictionary classResponses = new KeywordDictionary();
 
-        for (int i = 1; i < 6; i++) {
+        for (int i = classBeginningIndex; i < classEndingIndex; i++) {
             KeywordList keywordList = keywordExtractor.extractKeywords(inputMap.get(i).getResponse());
             classResponses.add(keywordList);
         }
@@ -22,7 +26,7 @@ public class ResponseProcessor {
     public KeywordDictionary processRaceResponseMap(HashMap<Integer, Response> inputMap) throws UnirestException {
         KeywordDictionary raceResponses = new KeywordDictionary();
 
-        for (int i = 6; i < 10; i++) {
+        for (int i = classEndingIndex; i < raceEndingIndex; i++) {
             KeywordList keywordList = keywordExtractor.extractKeywords(inputMap.get(i).getResponse());
             raceResponses.add(keywordList);
         }
